b5dc3cd56e696dce07c7f45b93931e709aeed705
significantnose <yanucevich.d@gmail.com>
2024-04-26 12:52:04 +0300

    Display of first message headers added

--- CS_and_N4/ViewModels/IMAPClientViewModel.cs
+++ CS_and_N4/ViewModels/IMAPClientViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CS_and_N4.Models;
 using DynamicData;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
@@ -7,59 +8,49 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
+using System.Linq;
 using System.Reactive;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CS_and_N4.ViewModels
 {
-    public class MailBox
-    {
-        public string Name { get; set; }
-        public string altName { get; set; }
-
-        public MailBox(string Name, string altName)
-        {
-            this.Name = Name;
-            this.altName = altName;
-        }
-    }
-    public class DialogueStruct
-    {
-        public string Query { get; set; }
-        public string Response { get; set; }
-        public DialogueStruct(string Query, string[] Response)
-        {
-            this.Query = Query;
-            StringBuilder sb = new StringBuilder();
-            foreach (string response in Response)
-            {
-                sb.Append(response);
-            }
-            this.Response = sb.ToString();
-        }
-    }
-    public class IMAPClientViewModel : ViewModelBase
+    public class IMAPClientViewModel : MailClientViewModelBase
     {
 
         public ReactiveCommand<Unit, Unit> QuitCommand { get; set; }
         protected IMAPClient client;
 
+        
+        private int _selectedMailboxIdx;
+        public int SelectedMailboxIdx
+        {
+            get => _selectedMailboxIdx;
+            set
+            {
+                // set the new contents of the mailbox mail list
+                SelectMailboxAsync(MailBoxes[value]);
+
+                this.RaiseAndSetIfChanged(ref _selectedMailboxIdx, value);
+            }
+        }
+
         [Reactive]
         public string CurrentContent { get; set; }
+        public ObservableCollection<MailMessage> CurrentMailList { get; set; } = new ObservableCollection<MailMessage>();
 
-        public ObservableCollection<MailBox> MailBoxes { get; set; }
-        public ObservableCollection<DialogueStruct> Log { get; set; }
-
+        public ObservableCollection<MailBox> MailBoxes { get; set; } = new ObservableCollection<MailBox>();
 
         public IMAPClientViewModel(IMAPClient client)
         {
             this.client = client;
-            MailBoxes = new ObservableCollection<MailBox>();
-            Log = new ObservableCollection<DialogueStruct>();
 
+            // acquiring the list of mailbox and making the 
+            // initially selected mailbox an empty one
             GetListOfMailboxesAsync();
+
 
             QuitCommand = ReactiveCommand.Create(() =>
             {
@@ -111,13 +102,213 @@ namespace CS_and_N4.ViewModels
                 }
 
                 MailBoxes.Clear();
+                MailBoxes.Add(new MailBox(MAILBOX_ALT_EMPTY, MAILBOX_EMPTY));
                 MailBoxes.AddRange(boxes);
+                SelectedMailboxIdx = 0;
                 // add to console log
-                Log.Add(new DialogueStruct(tag + " " + query, data));
+                //Log.Add(new DialogueStruct(tag + " " + query, data));
             }
         }
 
-        public async Task SelectMailboxAsync(string boxName) {
+        protected async Task SelectMailboxAsync(MailBox mb) {
+            if (mb.Name == MAILBOX_EMPTY && mb.altName == MAILBOX_ALT_EMPTY) return;
+
+
+            string[] selectResponse = await QuerySelectMailboxAsync(mb.Name);
+            if (selectResponse.Length > 0) {
+                int amntOfMail = 0;
+                foreach (string response in selectResponse)
+                {
+                    if (response.Contains("EXISTS")){
+                        // assuming that the string contains data of type: 
+                        // "* NNN EXISTS"
+                        string pattern = @"\* (\d+) EXISTS";
+                        Match match = Regex.Match(response, pattern);
+                        if (match.Success)
+                        {
+                            amntOfMail = int.Parse(match.Groups[1].Value);
+                        }
+                        else { 
+                            // error: string of other type
+                        }
+                        break;
+                    }
+                }
+                if (!(mb.MailCount != null && mb.MailCount == amntOfMail)) {
+                    mb.MailCount = amntOfMail;
+                    // now the max amount of pages is set
+                }
+
+
+                // display page; it'll deal with the unknown mail
+                DisplayMailPageAsync(mb, 0);
+            }
+            else { 
+                // error in response
+            }
+            // find the IDs if they are not found
+
+                // change the count of messages
+
+
+/*                int[] indices = await QuerySearchAllAsync();
+                foreach(int i in indices)
+                {
+                    mb.Mail.Add(i, null);
+                }*/
+            
+
+        }
+
+        protected async Task DisplayMailPageAsync(MailBox mb, int index) {
+            CurrentMailList.Clear();
+
+
+            if (mb.MailCount != null)
+            {
+                int maxPageIdx = mb.MaxPages - 1;
+                if (index > maxPageIdx) index = maxPageIdx;
+                if (index < 0) index = 0;
+
+                int startIdx = index * MailBox.mailPerPage+1;
+                int endIdx = (index + 1) * MailBox.mailPerPage;
+
+                if (endIdx > mb.MailCount) endIdx = mb.MailCount.Value;
+
+
+                // in order to not add these items to a CurrentMailList and 
+                // not make it re-appear every time
+                List<MailMessage> currMsgRange = new List<MailMessage>();
+
+                for (int i = startIdx; i <= endIdx; i++)
+                {
+                    // check if the current email is present
+                    if (!mb.Mail.ContainsKey(i))
+                    {
+                        // query the server for this range of messages
+                        // upd: what if there are only a few messages that need to be queried?
+                        // upd: imo it's better to ask for a range than to ask for each message separately
+
+
+                        // ask for headers 
+                        // then, update the 
+                        MailMessage[] mail = await QueryMsgHeaderAsync(startIdx, endIdx);
+                        if (mail.Length != endIdx - startIdx+1)
+                        {
+                            // error: some messages are not parsed
+                        }
+                        else {
+                            for (int j = startIdx; j <= endIdx; j++) {
+                                if (mb.Mail.ContainsKey(j))
+                                {
+                                    mb.Mail[j] = mail[j - startIdx];
+                                }
+                                else { 
+                                    mb.Mail.Add(j, mail[j-startIdx]);
+                                }
+                            }
+                        }
+
+                        break;
+                    }
+                }
+
+                for (int i = startIdx; i <= endIdx; i++) { 
+                    currMsgRange.Add(mb.Mail[i]);
+                }
+
+                CurrentMailList.AddRange(currMsgRange);
+            }
+            else { 
+                // unexpected error
+            }
+        }
+
+        protected async Task<MailMessage[]> QueryMsgHeaderAsync(int startIdx, int endIdx) {
+            string[] data;
+            string tag;
+
+            string query = $"FETCH {startIdx}:{endIdx} (BODY[HEADER.FIELDS (SUBJECT DATE)])";
+            (data, tag) = await client.SmalltalkAsync(query);
+
+            if (tag == "")
+            {
+                // error occurred
+                return [];
+            }
+            else
+            {
+                int counter = 0;
+                foreach (string line in data)
+                {
+                    Debug.WriteLine($"{counter}: {line}");
+                    counter++;
+                }
+
+                List<MailMessage> mailMsgs = new List<MailMessage>();
+                string date = "";
+                string subject = "";
+                for (int i = 1; i < data.Length-1; i++) {
+                    string currString = data[i];
+                    if (currString == ")")
+                    {
+                        mailMsgs.Add(new MailMessage(subject, date));
+
+                        date = "";
+                        subject = "";
+                    }
+                    else {
+                        if (currString.StartsWith("Date: "))
+                        {
+                            date = currString.Substring(5);
+                        }
+                        else if (currString.StartsWith("Subject: ")) {
+                            subject = currString.Substring(8);
+                        }
+                    }
+                }
+
+                // take the date string, remove it from the string poll
+                // the subject will be anything else
+
+                return mailMsgs.ToArray();
+            }
+        }
+
+        protected async Task<int[]> QuerySearchAllAsync()
+        {
+            string[] data;
+            string tag;
+
+            string query = "SEARCH ALL";
+            (data, tag) = await client.SmalltalkAsync(query);
+
+            if (tag == "")
+            {
+                // error occurred
+                return [];
+            }
+            else
+            {
+                // get the substring
+                if (data.Length != 2) {
+                    // not implemented: the mailbox is too large
+                    // wrong approach. must fix.
+                    throw new NotImplementedException();
+                }
+                string indicesResponse = data[0];
+                string keyword = "SEARCH ";
+                int idxStart = indicesResponse.IndexOf(keyword) + keyword.Length;
+                string indices = indicesResponse.Substring(idxStart);
+
+                // split it and convert it
+                string[] indicesStrings = indices.Split(' ');
+                int[] indicesValues = Array.ConvertAll(indicesStrings, int.Parse);
+                return indicesValues;
+            }
+        }
+
+        public async Task<string[]> QuerySelectMailboxAsync(string boxName) {
             string[] data;
             string tag;
 
@@ -127,6 +318,7 @@ namespace CS_and_N4.ViewModels
             if (tag == "")
             {
                 // error occurred
+                return [];
             }
             else {
                 int counter = 0;
@@ -134,6 +326,7 @@ namespace CS_and_N4.ViewModels
                     Debug.WriteLine($"{counter}: {line}");
                     counter++;
                 }
+                return data;
             }
         }
 
--- CS_and_N4/ViewModels/MailClientViewModelBase.cs
+++ CS_and_N4/ViewModels/MailClientViewModelBase.cs
@@ -1,17 +1,93 @@
 ﻿using CS_and_N4.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CS_and_N4.ViewModels
 {
+
+    public class MailMessage {
+        public string msgHeader { get; set; }
+        public string? msgDate { get; set; }
+        public string? msgBody { get; set; }
+
+        public MailMessage(string header, string? date = null, string? body = null)
+        {
+            msgHeader = header;
+            msgDate = date;
+            msgBody = body;
+        }
+    }
+    public class MailBox
+    {
+        public static readonly int mailPerPage = 5;
+        public string Name { get; set; }
+        public string altName { get; set; }
+
+        // this field is responsible for giving the answer if the
+        // SELECT command has been queried
+        // UPD: this is not necessary, as the mail that will be 
+        // displayed will contain the mail for the currently
+        // selected mailbox
+        // UPD UPD: the IDs must be known anyway
+        // UPD UPD UPD: if the dictionary keys are empty, that is the sign
+        //public int? amntOfMail = null;
+
+
+        public int MaxPages { get; private set; }
+
+        private int? _mailCount;
+        public int? MailCount { 
+            get => _mailCount; 
+            set { 
+                _mailCount = value;
+                if (value != null)
+                {
+                    MaxPages = (int)Math.Ceiling((double)value / mailPerPage);
+                }
+                else {
+                    // not valid value for MailCount to acquire MaxPages
+                    MaxPages = 0;
+                }
+            } 
+        }
+
+        public Dictionary<int,MailMessage?> Mail = new Dictionary<int,MailMessage?>();
+
+
+        public MailBox(string Name, string altName)
+        {
+            this.Name = Name;
+            this.altName = altName;
+            Mail = new Dictionary<int,MailMessage?>();
+        }
+    }
+    public class DialogueStruct
+    {
+        public string Query { get; set; }
+        public string Response { get; set; }
+        public DialogueStruct(string Query, string[] Response)
+        {
+            this.Query = Query;
+            StringBuilder sb = new StringBuilder();
+            foreach (string response in Response)
+            {
+                sb.Append(response);
+            }
+            this.Response = sb.ToString();
+        }
+    }
+
+
+
     public abstract class MailClientViewModelBase : ViewModelBase
     {
-        public ClientBase protocol;
-        // log
-        // mail list
-        // cached emails
+        protected static readonly string MAILBOX_EMPTY = "CSN4_NONE";
+        protected static readonly string MAILBOX_ALT_EMPTY = "CSN4_NONE";
+
+        public ObservableCollection<DialogueStruct> Log { get; set; } = new ObservableCollection<DialogueStruct>();
     }
 }
